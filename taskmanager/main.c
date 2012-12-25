bad68bf9df2fe154e509213fd69be9cc43777e77
Brett Jackson <brettbj.jackson@gmail.com>
2012-12-25 17:02:10 -0600

    Initial Commit

---
+++ taskmanager/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  taskmanager
+//
+//  Created by Brett Jackson on 12/25/12.
+//  Copyright (c) 2012 Brett Jackson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
